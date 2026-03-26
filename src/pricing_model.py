bb16c97d369237aa7a0ec50f82864d2b263969b5
Musa Mhlambi <m.mhlambi68@gmail.com>
2026-03-26 23:26:21 +0200

    Adjust pricing model parameters
    Updated pricing model to reflect new base fare and rates.

--- src/pricing_model.py
+++ src/pricing_model.py
@@ -1,16 +1,14 @@
 import numpy as np
 
 def calculate_price(df):
-    base_fare = 15
-    booking_fee = 4
-    rate_per_km = 11
-    rate_per_min = 0.9
+    base_fare = 12
+    rate_per_km = 6
+    rate_per_min = 0.7
 
     subtotal = (
         base_fare +
         df['distance_km'] * rate_per_km +
-        df['duration_min'] * rate_per_min +
-        booking_fee
-    )
+        df['duration_min'] * rate_per_min 
+        )
 
     return np.maximum(20, subtotal * df['surge_multiplier'])
