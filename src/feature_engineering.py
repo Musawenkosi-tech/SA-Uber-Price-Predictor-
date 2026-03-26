4d49d8c70038f32d1c8d98888c40340cd3a47dc0
Musa Mhlambi <m.mhlambi68@gmail.com>
2026-03-26 23:25:46 +0200

    Refine rush hour detection and surge pricing logic
    Updated rush hour logic to include additional hours and adjusted surge pricing calculations based on new masks for different time periods.

--- src/feature_engineering.py
+++ src/feature_engineering.py
@@ -14,19 +14,24 @@ def add_features(df):
     df['hour'] = df['pickup_datetime'].dt.hour
 
     # 4. Rush hour logic
-    df['is_rush_hour'] = df['hour'].isin([7, 8, 16, 17, 18])
+    df['is_rush_hour'] = df['hour'].isin([6, 7, 8, 9, 16, 17, 18,19])
+    mask = df['hour'].isin([0, 1, 2, 3, 4, 5, 10, 11, 12, 13, 14, 15, 19, 20, 21, 22, 23])
+    rush_mask = df['hour'].isin([6, 7, 8, 9])
+    evening_mask = df['hour'].isin([16, 17, 18])
+
 
     # 5. Traffic speed
     df['minute_per_km'] = np.where(
         df['is_rush_hour'],
         np.random.uniform(1.9, 2.3, len(df)),
-        np.random.uniform(1.3, 1.5, len(df))
+        np.random.uniform(1.3, 1.5, len(df))  
     )
 
     # 6. Duration
     df['duration_min'] = df['distance_km'] * df['minute_per_km']
 
     # 7. Surge pricing
-    df['surge_multiplier'] = np.where(df['is_rush_hour'], 1.4, 1.0)
-
+    df.loc[mask,'surge_multiplier'] = np.random.uniform(1.0, 1.2, mask.sum()) 
+    df.loc[rush_mask, 'surge_multiplier'] = np.random.uniform(1.3, 1.6, rush_mask.sum())
+    df.loc[evening_mask, 'surge_multiplier'] = np.random.uniform(1.4, 1.8, evening_mask.sum())
     return df
